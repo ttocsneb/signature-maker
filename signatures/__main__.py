51ed005a5afbb1d618352a024d38fa8475d23730
Benjamin Jacobs <benjammin1100@gmail.com>
2019-10-27 21:58:49 -0600

    number actual images

--- signatures/__main__.py
+++ signatures/__main__.py
@@ -1,11 +1,21 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='Process some integers.')
+parser = argparse.ArgumentParser(description='Number Pages for a book')
+
 parser.add_argument('papers', help="Papers per Signature", type=int)
 parser.add_argument('signatures', help="Number of Signatures", type=int)
+parser.add_argument('image', help="Background image for each paper", type=str)
+
+parser.add_argument('-t', '--top', dest='pos', help='Place the numbering on the top of the page', action='store_const', const='top', default='bot')
+parser.add_argument('-b', '--bottom', dest='pos', help='Place the numbering on the bottom of the page (default)', action='store_const', const='bot')
+parser.add_argument('-p', '--padding', dest='padding', metavar='px', help='padding from the edge of the numbers', type=int, default=16)
+parser.add_argument('-s', '--size', dest='size', metavar='pt', help='Size of the font', type=int, default=12)
+parser.add_argument('-f', '--font', dest='font', metavar='font', help='Font to use', type=str, default="arial")
+parser.add_argument('-o', '--output', dest='output', metavar='directory', help='output directory')
+parser.add_argument('-c', '--color', dest='color', metavar='color', help='set the color of the text', default='rgb(0, 0, 0)')
 
 args = parser.parse_args()
 
 from signatures import printBook
 
-printBook(args.signatures, args.papers)
+printBook(**vars(args))
--- signatures/signatures.py
+++ signatures/signatures.py
@@ -1,21 +1,82 @@
-counter = 1
+import os
+from os import path
+from PIL import Image, ImageFont, ImageDraw
 
 
-def getPage(left, right):
-    global counter
-    print(f"{counter}: {{{left}, {right}}}")
-    counter += 1
+def getPage(image, left, right, padding, pos, font, color, file):
+    canvas = ImageDraw.Draw(image)
+
+    w, h = image.size
+
+    # Left Number
+    width, height = canvas.textsize(str(left), font=font)
+
+    x = padding
+    y = padding
+    if pos == 'bot':
+        y = h - y - height
+
+    canvas.text((x, y), str(left), fill=color, font=font)
+
+    # Right Number
+
+    width, height = canvas.textsize(str(right), font=font)
+
+    x = w - width - padding
+    y = padding
+    if pos == 'bot':
+        y = h - y - height
+
+    canvas.text((x, y), str(right), fill=color, font=font)
+
+    print(f"{file}: [{left}, {right}]")
+
+    del canvas
+
+    image.save(file)
 
 
-def printSignature(start, size):
+def printSignature(image, start, size, padding, pos, font, color, folder):
     size *= 4
     for i in range(size // 2):
-        if i % 2:
-            getPage(start + i, start + size - i - 1)
-        else:
-            getPage(start + size - i - 1, start + i)
+        left = start + i
+        right = start + size - i - 1
+
+        # Swap the left and the right numbering
+        if not i % 2:
+            left, right = right, left
+        
+        f = path.join(folder, f"{i + start // 2 + 1}.png")
+
+        getPage(image.copy(), left, right, padding, pos, font, color, f)
 
 
-def printBook(signatures, sig_size):
+def printBook(papers, signatures, image, pos, padding, size, font, output, color, **kwargs):
+    image_file = image
+    font_file = font
+
+    f, f_ext = path.splitext(font_file)
+    if not f_ext:
+        f_ext = '.ttf'
+    font_file = ''.join([f, f_ext])
+
+    if output is None:
+        output = path.splitext(image)[0]
+
+    try:
+        font = ImageFont.truetype(font=font_file, size=size)
+    except IOError:
+        print(f"Could not find the font '{font_file}'.")
+        exit()
+
+    try:
+        image = Image.open(image_file)
+    except IOError as e:
+        print(e)
+        exit()
+
+    if not os.path.isdir(output):
+        os.mkdir(output)
+
     for i in range(signatures):
-        printSignature(i * sig_size * 4 + 1, sig_size)
+        printSignature(image, i * papers * 4 + 1, papers, padding, pos, font, color, output)
